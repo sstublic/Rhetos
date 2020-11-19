505027acc5feb508e2c699f5db311f781586d4a1
Sasa Stublic <sasa.stublic@gmail.com>
2020-11-20 00:06:45 +0100

    Draft WebApi demo.

---
+++ AspNetCoreDemo/WebApp/Controllers/RhetosApiController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Newtonsoft.Json;
+using Rhetos.Processing;
+using Rhetos.Processing.DefaultCommands;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class RhetosApiController : ControllerBase
+    {
+        private readonly IContainer rhetosContainer;
+
+        public RhetosApiController(IContainer rhetosContainer)
+        {
+            this.rhetosContainer = rhetosContainer;
+        }
+
+        [HttpGet]
+        public async Task<string> Alive()
+        {
+            return bool.TrueString;
+        }
+
+        [HttpGet]
+        public async Task<string> Claims()
+        {
+            var processingEngine = rhetosContainer.Resolve<IProcessingEngine>();
+
+            var readCommand = new ReadCommandInfo() {DataSource = "Common.Claim", ReadRecords = true};
+            var result = processingEngine.Execute(new List<ICommandInfo>() {readCommand});
+
+            return JsonConvert.SerializeObject(result, Formatting.Indented);
+        }
+
+    }
+}
---
+++ AspNetCoreDemo/WebApp/RhetosAspNetCoreServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Autofac;
+using Rhetos.Utilities;
+using WebApp;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class RhetosAspNetCoreServiceCollectionExtensions
+    {
+        public static IServiceCollection AddRhetos(this IServiceCollection serviceCollection, string rhetosApplicationFolder)
+        {
+            var logProvider = new ConsoleLogProvider();
+            var host = Rhetos.Host.Find(rhetosApplicationFolder, logProvider);
+            var rhetosConfiguration = host.RhetosRuntime.BuildConfiguration(logProvider, rhetosApplicationFolder, _ => { });
+            var rhetosContainer = host.RhetosRuntime.BuildContainer(logProvider, rhetosConfiguration, builder =>
+            {
+                builder.RegisterInstance(new DummyUserInfo()).As<IUserInfo>().SingleInstance();
+            });
+            serviceCollection.AddSingleton(rhetosContainer);
+
+            return serviceCollection;
+        }
+    }
+}
